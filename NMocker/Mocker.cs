da4d930bde063d42f30e4cbb4f281c615595ab94
Liangchen <leeonky@gmail.com>
2024-09-06 21:48:48 +0800

    Stub simlpe static method with no args

--- NMocker/Mocker.cs
+++ NMocker/Mocker.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace nmocker
 {
@@ -21,19 +23,35 @@ namespace nmocker
                 this.methodName = methodName;
             }
             private static Harmony harmony = new Harmony("Mocker");
-            private static object result;
+            private static StaticMockerAction staticMockerAction = new StaticMockerAction();
 
             public void ThenReturn(object value)
             {
-                StaticMocker.result = value;
                 HarmonyMethod prefix = new HarmonyMethod(typeof(StaticMocker).GetMethod("ReturnPrefix"));
-                harmony.Patch(type.GetMethod(methodName), prefix);
+                MethodInfo methodInfo = type.GetMethod(methodName);
+                staticMockerAction.Add(methodInfo, value);
+                harmony.Patch(methodInfo, prefix);
             }
 
-            public static bool ReturnPrefix(ref object __result)
+            public static bool ReturnPrefix(MethodBase __originalMethod, ref object[] __args, ref object __result)
             {
-                __result = StaticMocker.result;
+                __result = staticMockerAction.getResult(__originalMethod);
                 return false;
+            }
+        }
+
+        public class StaticMockerAction
+        {
+            private IDictionary<MethodBase, object> actions = new Dictionary<MethodBase, object>();
+
+            public void Add(MethodInfo method, object result)
+            {
+                actions.Add(method, result);
+            }
+
+            public object getResult(MethodBase method)
+            {
+                return actions[method];
             }
         }
     }
