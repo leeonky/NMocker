0889d7953a899f0c05449260d20a27f56e147dd7
Liangchen <leeonky@gmail.com>
2024-11-09 15:14:49 +0800

    UT for stub private setter

--- TestNMocker/StubPrivetStaticProperty.cs
+++ TestNMocker/StubPrivetStaticProperty.cs
@@ -8,29 +8,47 @@ namespace TestNMocker
     {
         public class Target
         {
+            public static int value;
             private static int PrivateProperty
             {
                 get { return 100; }
+                set { Target.value = value; }
             }
 
             public static int invokePrivate()
             {
                 return PrivateProperty;
             }
+
+            public static void invokePrivate(int i)
+            {
+                PrivateProperty = i;
+            }
         }
 
         [TestInitialize]
         public void setup()
         {
+            Target.value = 0;
             Mocker.Clear();
         }
 
         [TestMethod]
-        public void user_raw_arg_value_in()
+        public void stub_with_raw_arg_value()
         {
             Mocker.When(typeof(Target), "PrivateProperty").ThenReturn(1);
 
             Assert.AreEqual(1, Target.invokePrivate());
+        }
+
+        [TestMethod]
+        public void stub_setter()
+        {
+            Mocker.WhenVoid(typeof(Target), "PrivateProperty", 1).ThenDefault();
+
+            Target.invokePrivate(1);
+
+            Assert.AreNotEqual(1, Target.value);
         }
     }
 }
